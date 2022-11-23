3fc346ce473198e4b6fd1f775754193e088044af
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-11-23 10:35:49 +0800

    Add files via upload

---
+++ class2.java
@@ -0,0 +1,15 @@
+public class class2 {
+    public static void main(String[] args) {
+            int num=12,den=0;
+            int ans=num/den;
+            System.out.println("end of main()!!");
+        
+            if (den==0) {
+                System.out.println("除數為0");
+            }
+            else{
+                System.out.println(ans);
+            }
+
+    }
+}
